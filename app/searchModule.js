ca263577bf9f2270003dff338c13a8f00822a7b5
mistabenjo <benakehurst@hotmail.com>
2017-08-28 15:29:17 +0300

    v2
    completed refactor to angularJS

--- app/searchModule.js
+++ app/searchModule.js
@@ -1,13 +1,38 @@
-(function(){
+(function() {
 
-	"use strict";
+    "use strict";
 
-	var searchModule = angular.module("searchModule", []);
+    var searchModule = angular.module("searchModule", []);
 
-	searchModule.controller("SearchController", function($scope, $http){
+    searchModule.controller("SearchController", function($scope, $http) {
 
+    	var baseUrl = "http://api.giphy.com/v1/gifs/search?q=";
+    	var apiLimit = "&api_key=0ba59fa3a33748829f020bd4d8e4fd5d&limit=15";
+    	
+        var arr = [];
+        $scope.searches = arr;
 
+       	
+        $scope.getGif = function(userSearch){
+        	searchGif(userSearch).then(function(data){
+        		$scope.userGif = data.data;
+        		arr.push({term: userSearch});
+        		$scope.userSearch = '';
+        	});
+        }
 
-	});
+        function searchGif(userSearch) {
+            return $http({ 
+            	method: "GET", 
+            	url: baseUrl + userSearch + apiLimit,
+            	headers: { 'Content-Type': 'application/json' }
+            })
+            .then(function(response) {
+                var userSearchResponse = response.data;
+                return userSearchResponse;
+            });
+        };
+
+    });
 
 })();
